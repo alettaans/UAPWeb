3efbed5b103d1753d1fa5b4db0b07f15d2feb664
alettaans <aletta.ans12@gmail.com>
2023-12-28 11:43:33 +0700

    UAP WEB

---
+++ script.js
@@ -0,0 +1,14 @@
+let barang = [];
+
+function submit() {}
+
+function menambahkan() {
+  var kodebar = parseFloat.getBarangById(kodebar, jumlah);
+  var jumlah = parseFloat.getBarangById(kodebar, jumlah);
+
+  var hasil = kodebar + jumlah;
+}
+
+if (((jumlah = ""), (kodebar = ""))) {
+  alert("Kolom Tidak Boleh Kosong");
+}
